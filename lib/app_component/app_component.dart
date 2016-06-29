f4071633312798da6beb2b0f97765326d4e4ca28
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-06-30 01:19:24 +0300

    Add task list component

--- lib/app_component/app_component.dart
+++ lib/app_component/app_component.dart
@@ -1,6 +1,11 @@
 import 'package:angular2/core.dart';
 
 import 'package:ng2_task_manager/top_menu/top_menu_component.dart';
+import 'package:ng2_task_manager/tasks_list_component.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html', directives: const [TopMenuComponent])
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    directives: const [TopMenuComponent, TaskListComponent]
+)
 class AppComponent {}
---
+++ lib/tasks_list_component.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/core.dart';
+
+@Component(selector: 'task-list', templateUrl: 'tasks_list_component.html')
+class TaskListComponent {
+  List<String> taskList = ['123', '456'];
+}
