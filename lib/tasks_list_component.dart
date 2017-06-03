b78c032b6f586468b5a8942199f887f51cb485eb
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-06-03 15:13:44 +0300

    readme fix

--- lib/tasks_list_component.dart
+++ lib/tasks_list_component.dart
@@ -2,5 +2,5 @@ import 'package:angular2/core.dart';
 
 @Component(selector: 'task-list', templateUrl: 'tasks_list_component.html')
 class TaskListComponent {
-  List<String> taskList = ['123', '456'];
+  List<String> taskList = ['123', '456', 345];
 }
